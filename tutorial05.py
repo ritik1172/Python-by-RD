4b6afb0f2310e5b64aca30eadfde6d7363103b78
RitikDwivedi <ritikcu@gmail.com>
2026-05-28 17:47:05 +0530

    updated

---
+++ tutorial05.py
@@ -0,0 +1,18 @@
+# num = 20;
+
+# def hello():
+#     print("Functuion execution...")
+# hello()
+
+
+
+# a = 20; #Global variable --> place --> Global scope
+
+# def hello():
+#     print(a);
+
+# a = 30; #global variable
+# def hello():
+#     print(a);
+# hello() 
+    
---
+++ tutorial06.py
@@ -0,0 +1,39 @@
+#arithmetic operator
+# a = 10;
+# b = 20;
+# res = a + b;
+# print(res)
+
+#comparison operator
+# a = 20;
+# b = 20;
+
+# if(a==b):
+#     print("True")
+# else:
+#     print("false")
+
+
+#Membership
+# str = "hello";
+# print('r' in str);
+
+#identity
+# a = 20;
+# b = 40;
+# print(a is b)
+
+#logical
+# a = 10;
+# b = 20;
+
+# if a>10 or b>0:
+#     print("true")
+# else:
+#     print("false")
+
+
+
+
+
+
